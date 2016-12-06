3475828e97ca9a382edf35d9f1dc4a9d950c71e7
Axel Garcia <axelgar1123@gmail.com>
2016-12-05 22:15:02 -0800

    Initial Commit

---
+++ Assignment 6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 6
+//
+//  Created by Axel Garcia on 12/5/16.
+//  Copyright © 2016 Axel Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
